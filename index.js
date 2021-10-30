8601b3eec35e0120f54096645bcbd362c5d1bafc
Ariful islam <arifulislam2127@gmail.com>
2021-10-31 01:47:09 +0600

    added function

--- index.js
+++ index.js
@@ -1,15 +1,37 @@
 const express = require('express');
+const cors = require('cors')
+const { MongoClient } = require('mongodb');
+
 const app = express();
+require('dotenv').config()
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+app.use(express.json())
 
 
+const uri = "mongodb+srv://travellerUser:aeAp5uwAdwI1XwED@cluster0.dbdl3.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
+async function run () {
+    try{
+        await client.connect()
+        const database = client.db('online_Shop')
+        const ticketCollection = database.collection('tickets')
 
-
-
-
+        //get tickets api 
+        app.get('/tickets', async(req, res) =>{
+            const cursor = ticketCollection.find({});
+            const tickets = await cursor.toArray();
+            res.send(tickets)
+        })
+    }
+    finally{
+        // await client.close()
+    }
+}
+run().catch(console.dir)
 
 
 app.get('/', (req, res) => {
